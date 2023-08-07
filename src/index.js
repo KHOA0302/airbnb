cb3c5b35d1899a62db84be56b0f2d9e8f6e0378f
ngockhoa <khoai.t0302@gmail.com>
2023-08-07 09:29:57 +0700

    add transiont scaleheader, fill color scale-background

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-const header = document.querySelector("header");
+const header = document.querySelector(".header");
 const searchLocationBtn = document.querySelector(".search-location");
 const searchTimeBtn = document.querySelector(".search-time");
 const searchPeopleBtn = document.querySelector(".search-people");
 
 const headerSearchHandle = () => {
-  const headerContainer = header.querySelector(".header-container");
+  const headerParent = header.parentNode;
+
   header.style = "box-shadow: none";
   header.classList.add("header-scale");
+
+  const coverScreen = document.querySelector(".header-coverScreen");
+  coverScreen.classList.add("on");
 };
 
 searchLocationBtn.onclick = () => {
   headerSearchHandle();
+};
+
+header.parentNode.querySelector(".header-coverScreen").onclick = () => {
+  console.log("Im in");
 };
